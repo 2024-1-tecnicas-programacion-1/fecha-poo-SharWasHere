82e91ad4dceab7056ad0b15cf92ffc3ee2f60995
SharWasHere <montenegrosharik6@gmail.com>
2024-04-21 21:18:25 -0500

    ya puse mi codigo en principal :D

--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,26 @@
 package com.mycompany.fecha.poo;
 
+import java.util.Scanner;
+
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Escriba un día");
+        int diaUsuario = lector.nextInt();
+        System.out.println("Escriba un mes ");
+        int mesUsuario = lector.nextInt();
+        System.out.println("Escriba un año");
+        int anioUsuario = lector.nextInt();
+
+        Fecha fechaValida = new Fecha(diaUsuario, mesUsuario, anioUsuario);
+
+        if (fechaValida.validarFecha()) {
+            System.out.println("La fecha es válida");
+        } else {
+            System.out.println(fechaValida.fechaLarga());
+        }
+
+    }
+
 }
